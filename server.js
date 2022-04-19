1b3de253c1df692b036e93eb34837984757cebfe
Haku Matsunoki <ausecours1@gmail.com>
2022-04-19 21:48:19 +0300

    heroku fix 2

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config({ path: './config.env' });
 
@@ -19,6 +20,9 @@ const serverConfig = require('./configs/serverConfig');
 // EXPRESS APP =========================================
 const app = express();
 
+app.enable('trust proxy'); //heroku specific
+
+app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10kb' }));
